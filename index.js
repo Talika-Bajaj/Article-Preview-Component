f83275c84f6d6fd5d72a31217536327050c8a5d0
Talika Bajaj <bajajtalika2001@gmail.com>
2024-05-05 18:25:20 +0530

    Update files

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const share = document.getElementById('share');
+const shareBtn = document.getElementById('share-btn');
 
 share.addEventListener('click', displayOptions);
 share.addEventListener('click', displayOptionsWide);
+shareBtn.addEventListener('click', hideOptions);
 
 function displayOptions() {
-    if (screen.width <= 650) {
+    if (screen.width <= 950) {
         document.querySelector('.info').style.display = 'none';
         document.getElementById('share-options').style.display = 'flex';
     }
 }
 
+function hideOptions() {
+    if (screen.width <= 950) {
+        document.querySelector('.info').style.display = 'flex';
+        document.getElementById('share-options').style.display = 'none';
+    }
+}
+
 function displayOptionsWide() {
-    if (screen.width > 650) {
+    if (screen.width > 950) {
         if (document.getElementById('share-options').style.display === 'flex') {
             document.getElementById('share-options').style.display = 'none';
         } else {
